ef8874da4479f27e1d8769de6a2ce5e7b7bbf8ee
yousufislamme <online.okk@gmail.com>
2024-11-25 08:22:50 +0600

    fixing react-router v7_startTransition error

--- src/components/Dashboard/DashboardHome.jsx
+++ src/components/Dashboard/DashboardHome.jsx
@@ -7,7 +7,7 @@ const DashboardHome = () => {
          <div className="mt-10 grid grid-cols-[repeat(auto-fill,minmax(200px,1fr))] gap-5"> 
  <StatusGraph totalNumber="42233" percentNumber="67" yearlyCount="32451" subTitle="total users" upOrDown="up" /> 
  <StatusGraph totalNumber="23784728" percentNumber="43" yearlyCount="43445" subTitle="total users" /> 
- <StatusGraph totalNumber="44465632" percentNumber="33" yearlyCount="44554" subTitle="total users" upOrDown="down" /> <StatusGraph totalNumber="44465632" percentNumber="33" yearlyCount="44554" subTitle="total users" upOrDown="up" /> 
+ <StatusGraph totalNumber="44465632" percentNumber="33" yearlyCount="44554" subTitle="total users" upOrDown="up" /> <StatusGraph totalNumber="44465632" percentNumber="33" yearlyCount="44554" subTitle="total users" upOrDown="up" /> 
           
       </div>
 
